d6a89ead99cc9ddec3c6c6dcfea65b6e906fdb62
danya-sv <sarapulovdanila@gmail.com>
2024-12-28 20:21:08 +0600

    homework_7

--- main_app/settings.py
+++ main_app/settings.py
@@ -53,15 +53,6 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'main_app.urls'
 
 
-CACHES = {
-    'default': {
-        'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379',
-        'OPTIONS': {
-            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
-        },
-    }
-}
 
 TEMPLATES = [
     {
